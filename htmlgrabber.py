5075c078c3d9051c3a0a17bc4b1413597068f7fc
Andrew Parker <abparker@calpoly.edu>
2017-08-02 16:19:49 -0700

    html_grabber.py now takes a date object as an argument, outputs a list of strings, each string containing HTML of one agenda. Saves HTML to a single text file if run directly from command line.

--- htmlgrabber.py
+++ htmlgrabber.py
@@ -5,83 +5,98 @@
 
 import urllib2, sys
 from bs4 import BeautifulSoup
+from datetime import date
 
 # fix utf8 - related encoding errors
 reload(sys)
 sys.setdefaultencoding("utf8")
 
-def grabHTML(year, outputfile, appendToExisting):
-    """Collects all HTML-based agendas from sanjoseca.gov from the specified year and outputs the raw HTML of those agendas in a single file.
+def grabHTML(minDate):
+    """Collects all HTML-based agendas from sanjoseca.gov since the specified date and outputs the raw HTML of those agendas in a list of Strings.
     
     Args:
-        year: the year from which to gather Agendas.
-        outputfile: the name/path of the file in which you want to save the raw HTML
-        appendToExisting: (boolean) whether to overwrite outputfile, or append newly-scraped agendas. Useful if you want one file with all available years' agendas
+        minDate: a datetime.date object telling the function to ignore agendas with dates equal to or earlier than the given date.
     Returns:
-        void
-    Raises:
-        ValueError: if year is out of range (2015-2017)
-    
+        a list of Strings, each String containing all the raw HTML for a single agenda
     """
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     agendaURLs = {2017:"http://www.sanjoseca.gov/index.aspx?NID=5322", 2016:"http://www.sanjoseca.gov/index.aspx?NID=4858", 2015:"http://www.sanjoseca.gov/index.aspx?NID=4535"}
-    if year not in agendaURLs:
-        raise ValueError('Invalid year. HTML-based agendas are only to be found in 2015, 2016, and 2017.')
+    absoluteMinDate = date(2015, 5, 20)
+    if (minDate < absoluteMinDate):
+        minDate = absoluteMinDate
     
-    print "Scraping HTML agendas from year", str(year)+"..."
-    outputContent = ""
-    errorContent = str(year)+"\nnon-HTML agendas encountered:\n\n"
-    yearHTMLsoup = BeautifulSoup(urllib2.urlopen(agendaURLs[year]).read(), "html.parser")
-    linkTable = yearHTMLsoup.find("table", class_="telerik-reTable-2")
-    for row in linkTable.find_all("tr"):
-        anchor =  row.td.a
-        try:
-            a_href = anchor['href']
-            if (a_href.find("AgendaViewer") >= 0 and a_href.find(".pdf") == -1):
-                print "scraping " + a_href
-                outputContent += urllib2.urlopen(a_href).read()
-            else:
-                errorContent += anchor.string + "\t\t" + a_href + "\n"
-                print "*** non-HTML agenda found: " + anchor.string + "\t" + a_href
-        except TypeError:
-            pass
+    print "Scraping HTML agendas from after", str(minDate)+"..."
+    agendaHTMLs = []
     
-    if appendToExisting:
-        f = open(outputfile, 'a')
-    else:
-        f = open(outputfile, 'w')
-    err_f = open("htmlgrabber_error.txt", 'w')
-    f.write(outputContent)
-    err_f.write(errorContent)
-    f.close()
-    err_f.close()
-        
+    for year in agendaURLs.iterkeys():
+        if (year >= minDate.year):
+            print "Searching " + str(year) + "..."
+            yearHTMLsoup = BeautifulSoup(urllib2.urlopen(agendaURLs[year]).read(), "html.parser")
+            linkTable = yearHTMLsoup.find("table", class_="telerik-reTable-2")
+            for row in linkTable.find_all("tr"):
+                anchor =  row.td.a
+                month = 0
+                day = 0
+                try:
+                    a_href = anchor['href']
+                    a_text = anchor.text
+                    for i in range(len(months)):
+                        if a_text.startswith(months[i]):
+                            month = i+1
+                            break
+                    monthCharLength = len(months[month-1])
+                    day = int(a_text[monthCharLength+1:monthCharLength+3].rstrip(',- '))
+                        
+                    currentDate = date(year, month, day)
+                    if (currentDate > minDate):
+                        if (a_href.find("AgendaViewer") >= 0 and a_href.find(".pdf") == -1):
+                            print str(currentDate) + ": scraping " + a_href
+                            agendaHTMLs.append(urllib2.urlopen(a_href).read())
+                        else:
+                            print "*** non-HTML agenda found: " + anchor.string + "\t" + a_href
+                except TypeError:
+                    pass
+    
+    print "\nDone!\n\n"
+    return agendaHTMLs
     
 
 #command-line usability (run ""htmlgrabber.py -h" for help, or just read below....)
 if __name__ == '__main__':
     import getopt
-    y = 2017
+    d = date(2015,5,20)
     append = False
     output = "htmlgrabber_output.txt"
     try: 
-        opts, args = getopt.getopt(sys.argv[1:],"hy:o:a")
+        opts, args = getopt.getopt(sys.argv[1:],"hd:o:a")
     except getopt.GetoptError:
-        print "Error - please use this format:\n\tpython htmlgrabber.py -y <year> -o <outputfile.txt>"
+        print "Error - please use this format:\n\tpython htmlgrabber.py -d <YYYY/MM/DD> -o <outputfile.txt>"
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
             print "\nhtmlgrabber.py - command line interface\n\n\
 Collects all html-based agendas from the San Jose City Council website\n\
-in a given year, and spits them out as a single, continuous text file\n\
-(raw html). Also populates htmlgrabber_error.txt with any agendas unable\n\
-to be output, probably because that agenda is a pdf rather than html.\n\n\
-usage: htmlgrabber.py -y <year> -o <outputfile>\n\t\
+after the given date, and spits them out as a single, continuous text file\n\
+(raw html).\n\n\usage: htmlgrabber.py -d <YYYY/MM/DD> -o <outputfile.txt>\n\t\
 -a :\tappends new HTML to the existing outputfile\n\n"
             sys.exit()
-        elif opt == '-y':
-            y = int(arg)
+        elif opt == '-d':
+            year = int(arg[0:4])
+            month = int(arg[5:7])
+            day = int(arg[8:])
+            d = date(year, month, day)
         elif opt == '-o':
             output = arg
         elif opt == '-a':
             append = True
-    sys.exit(grabHTML(y, output, append))
+    agendas = grabHTML(d)
+    outputContent = ""
+    for a in agendas:
+        outputContent += a
+    if append:
+        f = open(output, 'a')
+    else:
+        f = open(output, 'w')
+    f.write(outputContent)
+    f.close()
+    sys.exit()
